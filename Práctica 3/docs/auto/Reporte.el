d28743133dc0eeba21c8f0b8000162018e8c455c
Diego Estrada <cron@localhost.localdomain>
2019-08-31 00:11:24 -0500

    Reporte terminado y algunos cambios en la tabla

---
+++ Práctica 3/docs/auto/Reporte.el
@@ -0,0 +1,47 @@
+(TeX-add-style-hook
+ "Reporte"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "letterpaper" "margin=1in" "footskip=0.25in")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "geometry")
+   (LaTeX-add-labels
+    "sec:orgd2f6e52"
+    "sec:orge9cb773"
+    "sec:org247eb51"
+    "sec:org36ac98a"
+    "sec:org15c4b37"
+    "sec:org532cd8d"
+    "sec:org20d51d3"
+    "sec:org687c26c"
+    "sec:org888d119"
+    "sec:orgc0a70e7"
+    "sec:org9c372e5"
+    "sec:org542e61c"))
+ :latex)
+
