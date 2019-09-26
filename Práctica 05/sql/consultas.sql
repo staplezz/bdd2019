526dbfc42d5e875abf86f15c38f0ef495dd05fbb
Jorge Francisco Cortés López <kokofrank@ciencias.unam.mx>
2019-09-25 23:35:05 -0500

    Update consultas.sql

--- Práctica 05/sql/consultas.sql
+++ Práctica 05/sql/consultas.sql
@@ -1,3 +1,8 @@
 --Conocer los datos de los clientes que tengan más de 21 años.
 SELECT * FROM Persona, date_part('year',age(fechaNac)) as edad
 WHERE edad > 21;
+
+--Conocer el puesto, nombre, edad y la fecha en la que inicio a trabajar de todos los choferes.
+--(Falta la fecha de inicio)                                        
+SELECT Persona.Nombre, date_part('year',age(fechaNac)) as edad FROM Persona
+JOIN Conductor ON Conductor.idPersona = Persona.idPersona;                                          
