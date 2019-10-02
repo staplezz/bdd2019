a093f8551b97d7719e64212e3b603739c2e34177
Jorge Francisco Cortés López <kokofrank@ciencias.unam.mx>
2019-10-02 16:02:46 -0500

    Create consultas.sql

---
+++ Práctica 06/sql/consultas.sql
@@ -0,0 +1,6 @@
+--Práctica 06: Fundamentos de Bases de Datos.
+
+-- 1: Conocer los datos de los automóviles que tengan más de 15 años en servicio.
+SELECT DISTINCT a.placas, marca, preciofactura, submarca, año, (date_part('year',age(m.fechaInic))) as añosDeServicio
+FROM automovil as a, manejar AS m
+WHERE (date_part('year',age(m.fechaInic))) > 15;
