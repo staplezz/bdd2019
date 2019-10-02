67492aaf6eaca3cc6a587f8268498cd4de4366d6
Jorge Francisco Cortés López <kokofrank@ciencias.unam.mx>
2019-10-02 18:13:36 -0500

    Update consultas.sql

--- Práctica 06/sql/consultas.sql
+++ Práctica 06/sql/consultas.sql
@@ -33,3 +33,53 @@ INNER JOIN
 ) clt
 ON cdt.idViaje = clt.idViaje
 ORDER BY conductor;                                
+
+--5: Conocer los automóviles que están contenidos en cada una de las categorías (Luxury SUV, Lite, Luxury).
+--Luxury SUV
+SELECT *
+FROM automovil
+WHERE preciofactura::money::numeric::float8 > 50000000.00;
+
+--Lite
+SELECT *
+FROM automovil
+WHERE preciofactura::money::numeric::float8 <= 44900000.00;
+
+--Luxury
+SELECT *
+FROM automovil
+WHERE preciofactura::money::numeric::float8 > 45000000.00;
+
+--6: Conocer cuales son los automóviles que pertenecen a mas de una categoría.
+SELECT *
+FROM automovil
+WHERE preciofactura::money::numeric::float8 >= 44900000.00;
+
+--7: Conocer la capacidad de pasajeros, el costo, categoría y modelo de cada uno de los automóviles.
+SELECT submarca,preciofactura,
+(CASE WHEN automovil.preciofactura::money::numeric::float8 <= 44900000.00 THEN 'Lite' 
+ WHEN automovil.preciofactura::money::numeric::float8 > 50000000.00 THEN 'Luxury SUV'
+ WHEN automovil.preciofactura::money::numeric::float8 > 45000000.00 THEN 'Luxury' ELSE 'n/a' END) AS categoria,
+ (CASE WHEN automovil.preciofactura::money::numeric::float8 <= 44900000.00 THEN 4 
+ WHEN automovil.preciofactura::money::numeric::float8 > 50000000.00 THEN 5
+ WHEN automovil.preciofactura::money::numeric::float8 > 45000000.00 THEN 4 ELSE 4 END) AS capacidad
+FROM automovil;
+
+--8: Conocer la forma de pago mas recurrente.
+SELECT formapago, count(*)
+FROM formadepago
+GROUP BY formapago
+ORDER BY count(*) DESC
+LIMIT 1;
+
+--9: Conocer el viaje mas caro.
+SELECT *
+FROM viaje
+ORDER BY costo DESC
+LIMIT 1;
+
+--10: Conocer el viaje mas corto.
+SELECT *
+FROM viaje
+ORDER BY tiempo ASC
+LIMIT 1;
